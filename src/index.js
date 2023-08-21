ecf8ead8773d7c7db8ba69b51c999dd0789885bd
kotBaun <kamkaliev@gmail.com>
2023-08-21 18:00:03 +0300

    init done console log in process config file

--- src/index.js
+++ src/index.js
@@ -1,18 +1,54 @@
-const {app, BrowserWindow} = require('electron');
+const { app, BrowserWindow } = require('electron');
 const path = require('path');
+const url = require('url')
+const fs = require('fs')
+
+
+let config = './src/config.json'
+
+const loadConfig = () => {
+  try {
+    if (fs.existsSync(config)) {
+
+      const configFile = fs.readFileSync(config, 'utf-8');
+      console.log(JSON.parse(configFile));
+      // return JSON.parse(configFile);
+    } else {
+      console.log('no file');
+    }
+
+  } catch (error) {
+    console.error('Error loading config:', error);
+    return {}; // Если файл не существует или возникла ошибка, вернуть пустой объект
+  }
+}
 
 const createWindow = () => {
+  console.log(app.getPath('userData'));
   const win = new BrowserWindow({
     width: 300,
     height: 500,
-    icon: path.join(__dirname, 'icon.png'),
+    icon: path.join(__dirname, './calculator/icon.png'),
     resizable: false
   });
   win.setMenuBarVisibility(false);
   win.setTitle('Калькулятор');
-  win.loadFile('src/index.html');
+  // win.loadFile('src/index.html');
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, './calculator/calculator.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
 }
 
-app.whenReady().then(() => createWindow());
+
+
+
+
+
+app.whenReady().then(() => {
+  loadConfig();
+  createWindow();
+});
 app.on('window-all-closed', () => app.quit());
 //tttttt
